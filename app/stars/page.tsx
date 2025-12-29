5f0fb64a37e4e40ac27a436e36238f0d32f4fd66
neragit <nerazd@hotmail.com>
2025-12-29 01:28:50 +0100

    testing axis

--- app/stars/page.tsx
+++ app/stars/page.tsx
@@ -698,7 +698,7 @@ export default function Page() {
               position: "absolute",
               left: "50%",
               top: "50%",
-              transform: "translate(-200px, -230px) rotate(-90deg)",
+              transform: "translate(-200px, -200px) rotate(-90deg)",
               transformOrigin: "right center",
               minWidth: "8rem",
               textAlign: "center",
